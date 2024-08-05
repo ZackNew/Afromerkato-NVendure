439667c0a4ed342bf58f4fdb32fdc53414b965dc
Zack <zekariyasmulugeta91@gmail.com>
2024-08-05 08:46:02 +0300

    feat: codegen configuration added.

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,17 +3,27 @@ export {};
 // Here we declare the members of the process.env object, so that we
 // can use them in our application code in a type-safe manner.
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            APP_ENV: string;
-            COOKIE_SECRET: string;
-            SUPERADMIN_USERNAME: string;
-            SUPERADMIN_PASSWORD: string;
-            DB_HOST: string;
-            DB_PORT: number;
-            DB_NAME: string;
-            DB_USERNAME: string;
-            DB_PASSWORD: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      APP_ENV: string;
+      COOKIE_SECRET: string;
+      SUPERADMIN_USERNAME: string;
+      SUPERADMIN_PASSWORD: string;
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_NAME: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      ECOMMERCE_SERVER_NAME: string;
+
+      // Email
+      SMTP_HOST: string;
+      SMTP_PORT: string;
+      SMTP_USER: string;
+      SMTP_PASS: string;
+
+      // Arif pay credentials
+      ARIFPAY_KEY: string;
     }
+  }
 }
