7752bb70e36d6de95141b018f62d91c9e0e13131
Zack <zekariyasmulugeta91@gmail.com>
2024-08-12 16:35:59 +0300

    some changes

--- src/plugins/reports/services/refund-admin-api.service.ts
+++ src/plugins/reports/services/refund-admin-api.service.ts
@@ -1,30 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { ChannelService, ID, ListQueryBuilder, ListQueryOptions, PaginatedList, Refund, RequestContext, TransactionalConnection, UserService } from '@vendure/core';
+import { Injectable } from "@nestjs/common";
+import {
+  ChannelService,
+  ID,
+  ListQueryBuilder,
+  ListQueryOptions,
+  PaginatedList,
+  Refund,
+  RequestContext,
+  TransactionalConnection,
+  UserService,
+} from "@vendure/core";
 
 @Injectable()
-export class RefundReportService{
-    constructor(private connection: TransactionalConnection, 
-      private channelService: ChannelService,
-      private readonly listQueryBuilder: ListQueryBuilder,
-      private userService: UserService,){
-    }
+export class RefundReportService {
+  constructor(
+    private connection: TransactionalConnection,
+    private channelService: ChannelService,
+    private readonly listQueryBuilder: ListQueryBuilder,
+    private userService: UserService
+  ) {}
 
-    async findAll(ctx: RequestContext, options?: ListQueryOptions<Refund>): Promise<PaginatedList<Refund>> {
-      const result= await this.listQueryBuilder
-          .build(Refund, options, { 
-             ctx,
-             where:({
-               'state':"Settled"
-             }),
-             orderBy:{createdAt:'DESC'}})
-             .innerJoinAndSelect("refund.payment", "payment")
-             .innerJoinAndSelect("payment.order", "order")
-          .getManyAndCount()
-          .then(([items, totalItems]) => ({ items, totalItems }));
+  async findAll(
+    ctx: RequestContext,
+    options?: ListQueryOptions<Refund>
+  ): Promise<PaginatedList<Refund>> {
+    const result = await this.listQueryBuilder
+      .build(Refund, options, {
+        ctx,
+        where: {
+          state: "Settled",
+        },
+        orderBy: { createdAt: "DESC" },
+      })
+      .innerJoinAndSelect("refund.payment", "payment")
+      .innerJoinAndSelect("payment.order", "order")
+      .getManyAndCount()
+      .then(([items, totalItems]) => ({ items, totalItems }));
 
-          return result;
-
+    return result;
   }
-
-   
 }
